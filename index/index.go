b81d227c90c9c55087b40dd22d7725ae8217d56a
shimanekb <brandon.j.shimanek@gmail.com>
2021-03-17 15:33:40 -0500

    index and data log added

--- index/index.go
+++ index/index.go
@@ -1,5 +1,100 @@
 package kvstore
 
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+type DataLog interface {
+	Read(offset int64) (logItem *LogItem, err error)
+	Add(key string, value string) (offset int64, err error)
+}
+
+type LogItem struct {
+	key   string
+	value string
+	size  int64
+}
+
+func (l *LogItem) Key() string {
+	return l.key
+}
+
+func (l *LogItem) Value() string {
+	return l.value
+}
+
+func (l *LogItem) Size() int64 {
+	return l.size
+}
+
+func NewLogItem(key string, value string, size int64) LogItem {
+	return LogItem{key, value, size}
+}
+
+type LocalDataLog struct {
+	flushThreshold int
+	filePath       string
+}
+
+func NewLocalDataLog(filePath string) DataLog {
+	dataLog := LocalDataLog{10, filePath}
+	return &dataLog
+}
+
+func (l *LocalDataLog) Read(offset int64) (logItem *LogItem, err error) {
+	storeFile, err := os.Open(l.filePath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer storeFile.Close()
+
+	_, err = storeFile.Seek(offset, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	reader := csv.NewReader(storeFile)
+	record, err := reader.Read()
+
+	if err != nil {
+		return nil, err
+	}
+
+	key := record[0]
+	value := record[1]
+	size := int64(len([]byte(value)))
+
+	li := NewLogItem(key, value, size)
+	return &li, nil
+}
+
+func (l *LocalDataLog) Add(key string, value string) (offset int64, err error) {
+	file, err := os.OpenFile(l.filePath, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+	defer file.Close()
+	if err != nil {
+		return 0, err
+	}
+
+	size := int64(len([]byte(value)))
+	length, write_err := file.WriteString(fmt.Sprintf("%s,%s,%d,\n", key, value, size))
+
+	if write_err != nil {
+		return 0, write_err
+	}
+
+	fi, statErr := file.Stat()
+	if statErr != nil {
+		return 0, statErr
+	}
+
+	offset = fi.Size() - int64(length)
+	return offset, nil
+}
+
 type IndexItem struct {
 	offset int64
 	size   int64
@@ -21,6 +116,7 @@ type Index interface {
 	Get(key string) (indexItems []IndexItem, ok bool)
 	Put(key string, indexItem IndexItem)
 	Del(key string)
+	DataLog() DataLog
 }
 
 func getPartialKey(key string) string {
@@ -35,6 +131,7 @@ func getPartialKey(key string) string {
 type LocalIndex struct {
 	storageFilePath string
 	indexItems      map[string][]IndexItem
+	localDataLog    DataLog
 }
 
 func (i *LocalIndex) Get(key string) (indexItems []IndexItem, ok bool) {
@@ -53,7 +150,6 @@ func (i *LocalIndex) Put(key string, indexItem IndexItem) {
 }
 
 func (i *LocalIndex) Del(key string) {
-	path := i.storageFilePath
 	indexItems, ok := i.Get(key)
 
 	if !ok {
@@ -61,12 +157,13 @@ func (i *LocalIndex) Del(key string) {
 	}
 
 	for index, item := range indexItems {
-		k, err := readKey(path, item.offset)
+
+		logItem, err := i.localDataLog.Read(item.Offset())
 		if err != nil {
 			break
 		}
 
-		if key == k {
+		if logItem.Key() == key {
 			indexItems[index] = indexItems[len(indexItems)-1]
 			i.indexItems[getPartialKey(key)] = indexItems[:len(indexItems)-1]
 			continue
@@ -74,10 +171,9 @@ func (i *LocalIndex) Del(key string) {
 	}
 }
 
-func readKey(filepath string, offset int64) (key string, err error) {
-}
-
-func NewLocalIndex(storageFilePath string) Index {
+func NewLocalIndex(storageFilePath string, dataLog DataLog) Index {
 	indexItems := make(map[string][]IndexItem)
-	return &LocalIndex{storageFilePath, indexItems}
+	localIndex := LocalIndex{storageFilePath, indexItems, dataLog}
+
+	return &localIndex
 }
