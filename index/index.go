d2167212345c2cf4ea56428762557fbd6d56369f
shimanekb <brandon.j.shimanek@gmail.com>
2021-03-18 07:28:08 -0500

    save added

--- index/index.go
+++ index/index.go
@@ -1,16 +1,30 @@
 package index
 
+import (
+	"encoding/csv"
+	"fmt"
+	"math/rand"
+	"os"
+	"sort"
+)
+
 type IndexItem struct {
-	offset int64
-	size   int64
+	partialKey string
+	offset     int64
+	size       int64
 }
 
-func NewIndexItem(offset int64, size int64) IndexItem {
-	return IndexItem{offset, size}
+func NewIndexItem(key string, offset int64, size int64) IndexItem {
+	pk := getPartialKey(key)
+	return IndexItem{pk, offset, size}
 }
 
 func (i *IndexItem) Size() int64 {
 	return i.size
+}
+
+func (i *IndexItem) PartialKey() string {
+	return i.partialKey
 }
 
 func (i *IndexItem) Offset() int64 {
@@ -22,6 +36,7 @@ type Index interface {
 	Put(key string, indexItem IndexItem)
 	Del(key string)
 	DataLog() DataLog
+	Save() error
 }
 
 func getPartialKey(key string) string {
@@ -39,14 +54,51 @@ type LocalIndex struct {
 	localDataLog    DataLog
 }
 
+func (i *LocalIndex) Save() error {
+	var items []IndexItem
+	iitems := i.indexItems
+	for _, value := range iitems {
+		for _, it := range value {
+			items = append(items, it)
+		}
+	}
+
+	sort.Slice(items, func(i, j int) bool {
+		return items[i].Offset() < items[j].Offset()
+	})
+
+	fileName := fmt.Sprintf("index_swap%d.csv", rand.Int())
+	file, err := os.Open(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	for _, item := range items {
+		var record []string
+		record = append(record, item.PartialKey())
+		record = append(record, string(item.Offset()))
+		record = append(record, string(item.Size()))
+
+		writer.Write(record)
+	}
+
+	writer.Flush()
+
+	err = os.Rename(fileName, i.storageFilePath)
+
+	return err
+}
+
 func (i *LocalIndex) Get(key string) (indexItems []IndexItem, ok bool) {
 	partialKey := getPartialKey(key)
 	indexItems, ok = i.indexItems[partialKey]
 	return indexItems, ok
 }
 
-func (i *LocalIndex) Put(key string, indexItem IndexItem) {
-	indexItems, ok := i.indexItems[key]
+func (i *LocalIndex) Put(indexItem IndexItem) {
+	indexItems, ok := i.indexItems[indexItem.PartialKey()]
 	if !ok {
 		indexItems = make([]IndexItem, 0)
 	}
@@ -79,6 +131,8 @@ func (i *LocalIndex) Del(key string) {
 func NewLocalIndex(storageFilePath string, dataLog DataLog) Index {
 	indexItems := make(map[string][]IndexItem)
 	localIndex := LocalIndex{storageFilePath, indexItems, dataLog}
+
+	//Load index if exist
 
 	return &localIndex
 }
